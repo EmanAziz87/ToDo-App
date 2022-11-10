6ca86dce9661e40334ac36bbfd72fded212cd1ec
EmanAziz87 <aziz.eman@hotmail.com>
2022-11-10 12:57:45 -0500

    couldnt edit todos in the middle of a list without breaking the order and contents of surrounding todos. the issue is now fixed

--- src/components/displayToDoCard/displayToDoCard.js
+++ src/components/displayToDoCard/displayToDoCard.js
@@ -1,5 +1,6 @@
 import { currentProject, navContainer, projectManager } from '../displayNavMenu/displayNavMenu';
-import { projectsArr, storageToDo } from '../manageToDo/manageToDo';
+import { editModeContainer, projectsArr, storageToDo } from '../manageToDo/manageToDo';
+import threeDots from './3-vertical-dots-icon.svg';
 import './displayToDoCard.css';
 
 const mainContent = document.querySelector('#content');
@@ -7,7 +8,6 @@ const toDoCardAreaBackground = document.createElement('article');
 const toDoCardAreaMain = document.createElement('div');
 const toDoCardContainer = document.createElement('section');
 const selectedProjectHeader = document.createElement('p');
-
 
 
 function cardArea () {
@@ -31,7 +31,52 @@ function cardArea () {
     projectManager();
 };
 
- 
+
+function editDropDownMenu(toDoCard, editIcon) {
+    let editModeState = false;
+    const dropDowncontainer = document.createElement('div');
+    const editOption = document.createElement('button');
+    const deleteOption = document.createElement('button');
+
+    toDoCard.appendChild(dropDowncontainer);
+    dropDowncontainer.appendChild(editOption);
+    dropDowncontainer.appendChild(deleteOption);
+    
+    dropDowncontainer.classList.add('drop-down-container');
+    editOption.classList.add('edit-option', 'card-dropdown');
+    deleteOption.classList.add('delete-option', 'card-dropdown');
+
+    editOption.textContent = 'Edit';
+    deleteOption.textContent = 'Remove';
+
+    editIcon.addEventListener('click', function() {
+        if (!editModeState) {
+            dropDowncontainer.style.display = 'flex';
+            const editedToDo = editIcon.parentNode.childNodes[1].childNodes[1].childNodes[0].textContent;
+            editModeState = true; 
+            deleteOption.addEventListener('click', function() {
+                storageToDo.removeToDo(editedToDo)
+                editIcon.parentNode.remove();
+                console.log(projectsArr);
+            });
+
+            editOption.addEventListener('click', function() {
+                const editedToDo = editIcon.parentNode.childNodes[1].childNodes[1].childNodes[0].textContent;
+                while (toDoCard.firstChild) {
+                    toDoCard.removeChild(toDoCard.lastChild);
+                };
+                editModeContainer(toDoCard, editedToDo);
+                
+
+            });
+
+        } else if (editModeState) {
+            editModeState = false;
+            dropDowncontainer.style.display = 'none'; 
+        }
+    });
+    
+}
  
 function displayToDoCard(currentProjectIndex, projectArr) {
     for (let i = projectArr[currentProjectIndex].toDos.length - 1; i < projectArr[currentProjectIndex].toDos.length; i++ ) {
@@ -42,21 +87,26 @@ function displayToDoCard(currentProjectIndex, projectArr) {
             const cardNameAndDescription = document.createElement('div');
             const cardDueDateAndPriority = document.createElement('div');
             const checkBox = document.createElement('button');
+            const editIcon = document.createElement('img');
+
+            editIcon.src = threeDots;
             
             toDoCardContainer.appendChild(toDoCard);
             toDoCard.appendChild(checkBox);
             toDoCard.appendChild(cardContentSubContainer);
+            toDoCard.appendChild(editIcon);
             cardContentSubContainer.appendChild(cardDueDateAndPriority);
-            cardContentSubContainer.appendChild(cardNameAndDescription)
-            
-            
+            cardContentSubContainer.appendChild(cardNameAndDescription);
             checkBox.textContent = '✓';
             
-            toDoCard.classList.add('todo-card', 'todo-card');
+            toDoCard.classList.add('todo-card');
             checkBox.classList.add(`todo-checkbox-${i}`, 'todo-checkbox');
             cardContentSubContainer.classList.add('card-content-subcontainer');
             cardDueDateAndPriority.classList.add('card-due-date-and-priority');
-            
+            editIcon.classList.add('edit-card-icon');
+
+            editDropDownMenu(toDoCard, editIcon);
+
             let j = 0;
             
             for (const property in projectArr[currentProjectIndex].toDos[i]) {
@@ -80,6 +130,63 @@ function displayToDoCard(currentProjectIndex, projectArr) {
 
 }
 
+function displayEditedToDoCard(currentProjectIndex, projectArr, toDoIndex, editedToDo, toDoCard) {
+    for (let i = toDoIndex; i < toDoIndex + 1; i++ ) {
+        if (projectArr[currentProjectIndex].toDos.length > 0) {
+            const newToDoCard = document.createElement('div');
+            const cardContentSubContainer = document.createElement('div');
+            const cardNameAndDescription = document.createElement('div');
+            const cardDueDateAndPriority = document.createElement('div');
+            const checkBox = document.createElement('button');
+            const editIcon = document.createElement('img');
+
+            editIcon.src = threeDots;
+            
+            toDoCardContainer.insertBefore(newToDoCard, toDoCard);
+            newToDoCard.appendChild(checkBox);
+            newToDoCard.appendChild(cardContentSubContainer);
+            newToDoCard.appendChild(editIcon);
+            cardContentSubContainer.appendChild(cardDueDateAndPriority);
+            cardContentSubContainer.appendChild(cardNameAndDescription);
+            checkBox.textContent = '✓';
+            
+            newToDoCard.classList.add('todo-card');
+            checkBox.classList.add('todo-checkbox');
+            cardContentSubContainer.classList.add('card-content-subcontainer');
+            cardDueDateAndPriority.classList.add('card-due-date-and-priority');
+            editIcon.classList.add('edit-card-icon');
+
+            toDoCard.remove();
+
+            let j = 0;
+            console.log("hey");
+            for (const property in projectArr[currentProjectIndex].toDos[toDoIndex]) {
+                console.log('first step of the loop');
+                const toDoCardContent = document.createElement('div');
+                const todoClassNamesArr = ['todo-name', 'todo-due-date'];
+                
+                toDoCardContent.classList.add(todoClassNamesArr[j]);
+                
+                toDoCardContent.textContent = projectArr[currentProjectIndex].toDos[i][property];
+                console.log(toDoCardContent.textContent);
+                if (property === 'name') {
+                    cardNameAndDescription.appendChild(toDoCardContent);
+                    
+                } else if (property === 'dueDate') {
+                    cardDueDateAndPriority.appendChild(toDoCardContent);
+                }
+                j++; 
+            }
+
+            editDropDownMenu(newToDoCard, editIcon);
+
+            
+        }
+    
+    }
+
+}
+
 function recycleThroughTodos(currentProjectIndex, projectArr) {
     for (let i = 0; i < projectArr[currentProjectIndex].toDos.length; i++ ) {
         if ((projectArr[currentProjectIndex].toDos.length) > 0) {
@@ -89,12 +196,16 @@ function recycleThroughTodos(currentProjectIndex, projectArr) {
             const cardNameAndDescription = document.createElement('div');
             const cardDueDateAndPriority = document.createElement('div');
             const checkBox = document.createElement('button');
+            const editIcon = document.createElement('img');
+
+            editIcon.src = threeDots;
     
             toDoCardContainer.appendChild(toDoCard);
             toDoCard.appendChild(checkBox);
             toDoCard.appendChild(cardContentSubContainer);
+            toDoCard.appendChild(editIcon);
             cardContentSubContainer.appendChild(cardDueDateAndPriority);
-            cardContentSubContainer.appendChild(cardNameAndDescription)
+            cardContentSubContainer.appendChild(cardNameAndDescription);
 
     
             checkBox.textContent = '✓';
@@ -103,7 +214,11 @@ function recycleThroughTodos(currentProjectIndex, projectArr) {
             checkBox.classList.add(`todo-checkbox-${i}`, 'todo-checkbox');
             cardContentSubContainer.classList.add('card-content-subcontainer');
             cardDueDateAndPriority.classList.add('card-due-date-and-priority');
-    
+            editIcon.classList.add('edit-card-icon');
+
+            
+            editDropDownMenu(toDoCard, editIcon);
+
             let j = 0;
 
             for (const property in projectArr[currentProjectIndex].toDos[i]) {
@@ -117,7 +232,7 @@ function recycleThroughTodos(currentProjectIndex, projectArr) {
                 if (property === 'name') {
                     cardNameAndDescription.appendChild(toDoCardContent);
                     
-                } else {
+                } else if (property === 'dueDate') {
                     cardDueDateAndPriority.appendChild(toDoCardContent);
                 }
                 
@@ -182,5 +297,6 @@ export  {
     toDoCardAreaMain, 
     toDoCardContainer, 
     selectedProjectHeader, 
-    recycleThroughTodos
+    recycleThroughTodos,
+    displayEditedToDoCard
 } 
